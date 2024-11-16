61ad1e18a1453b8df55ce3568c3c29ff17c5b235
CybXII <luft.luft86@googlemail.com>
2024-11-16 20:52:09 +0100

    added customer profile and business profile seriallizer

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BusinessProfileView, CustomerProfileView
 
 urlpatterns = [    
-    path('business/<int:pk>/', BusinessProfileView.as_view()),
-    path('customer/<int:pk>/', CustomerProfileView.as_view()),
+    path('business/', BusinessProfileView.as_view()),
+    path('customer/', CustomerProfileView.as_view()),
 ]
