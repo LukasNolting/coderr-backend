07a1e7e3d89ce00cdf3f1db780de56ceffac512b
BenjaminTietz <mail@benjamin-tietz.com>
2024-11-17 18:22:16 +0100

    fixed profile view url

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BusinessProfileView, CustomerProfileView
+from .views import BusinessProfileView, CustomerProfileView, ProfileView
 
-urlpatterns = [    
-    path('business/', BusinessProfileView.as_view()),
-    path('customer/', CustomerProfileView.as_view()),
+urlpatterns = [
+    path('business/', BusinessProfileView.as_view(), name='business-profile'),
+    path('customer/', CustomerProfileView.as_view(), name='customer-profile'),
+    path('<int:pk>/', ProfileView.as_view(), name='profile-detail'), 
 ]
