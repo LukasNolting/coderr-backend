188fd5a37cdd0953a84686d955baec949fd004e9
BenjaminTietz <mail@benjamin-tietz.com>
2024-11-17 17:00:04 +0100

    orders_app

--- coderr_app/urls.py
+++ coderr_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from .views import BaseInfoView
 from offers_app.views import OfferDetailView
-from orders_app.views import OrderCountView, CompletedOrderCountView
 from profile_app.views import BusinessProfileView, CustomerProfileView
 from auth_app.views import LoginView, RegisterView
 
@@ -11,8 +10,6 @@ urlpatterns = [
     path('offers/', include('offers_app.urls')),   
     path('offerdetails/<int:pk>', OfferDetailView.as_view()),
     path('orders/', include('orders_app.urls')),
-    path('order-count/<int:pk>', OrderCountView.as_view()),
-    path('completed-order-count/<int:pk>', CompletedOrderCountView.as_view()),
     path('base-info/', BaseInfoView.as_view()),
     path('profile/', include('profile_app.urls')),
     path('profiles/business/', BusinessProfileView.as_view()),
