79f1eab9fd663c21f21693b88550a5671b5ff790
BenjaminTietz <mail@benjamin-tietz.com>
2024-11-19 16:48:09 +0100

    adjusted admin panel and changed user model to CustomUser

--- coderr_app/admin.py
+++ coderr_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from auth_app.models import CustomUser, PasswordReset
 from offers_app.models import Offer, OfferDetail
+from orders_app.models import Order
+from review_app.models import Review
+
 
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
@@ -24,3 +27,13 @@ class OfferAdmin(admin.ModelAdmin):
 @admin.register(OfferDetail)
 class OfferDetailAdmin(admin.ModelAdmin):
     list_display = ('title', 'offer_type', 'offer', 'price', 'delivery_time_in_days','revisions')
+    
+@admin.register(Order)
+class OrderDetailAmin(admin.ModelAdmin):
+    list_display = ('customer_user', 'business_user', 'title', 'status', 'created_at', 'updated_at')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('business_user', 'reviewer_id', 'rating', 'created_at', 'updated_at')
+    search_fields = ('business_user__username', 'reviewer_id__username', 'rating', 'description')
+    list_filter = ('rating', 'created_at')
