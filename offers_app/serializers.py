463b21c0ca9882d73a3469aa8f200662f3e7eca2
CybXII <luft.luft86@googlemail.com>
2024-11-20 02:31:43 +0100

    -bug fix sorting functions -bug fix frontend / backend price value error

--- offers_app/serializers.py
+++ offers_app/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import Offer, OfferDetail
-
+from decimal import Decimal
 
 class OfferDetailSerializer(serializers.ModelSerializer):
+    price = serializers.SerializerMethodField()
+
     class Meta:
         model = OfferDetail
         fields = [
@@ -15,12 +17,18 @@ class OfferDetailSerializer(serializers.ModelSerializer):
             'offer_type',
         ]
 
+    def get_price(self, obj):
+        try:
+            return f"{Decimal(obj.price):.2f}"  # Erzwingt, dass der Preis als Decimal behandelt wird
+        except (ValueError, TypeError):
+            return "0.00"
 
 class OfferSerializer(serializers.ModelSerializer):
     details = OfferDetailSerializer(many=True)
-    min_price = serializers.SerializerMethodField()
+    min_price = serializers.SerializerMethodField()  # Ändern zu SerializerMethodField
     min_delivery_time = serializers.SerializerMethodField()
     user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Offer
         fields = [
@@ -39,21 +47,26 @@ class OfferSerializer(serializers.ModelSerializer):
 
     def get_min_price(self, obj):
         """
-        Berechnet den minimalen Preis aus den verknüpften OfferDetails.
+        Berechnet den minimalen Preis aus den verknüpften OfferDetails
+        und formatiert ihn auf zwei Nachkommastellen.
         """
-        return obj.get_min_price()
+        min_price = obj.get_min_price()
+        if min_price is not None:
+            return "{:.2f}".format(min_price)  # Erzwinge 2 Nachkommastellen
+        return "0.00"
 
     def get_min_delivery_time(self, obj):
         """
         Berechnet die minimale Lieferzeit aus den verknüpften OfferDetails.
         """
-        return obj.get_min_delivery_time()
+        min_delivery_time = obj.get_min_delivery_time()
+        return min_delivery_time if min_delivery_time is not None else 0
 
     def create(self, validated_data):
         details_data = validated_data.pop('details', [])
         if len(details_data) != 3:
             raise serializers.ValidationError("Es müssen genau drei Angebotsdetails angegeben werden.")
-        
+
         offer = Offer.objects.create(**validated_data)
         for detail_data in details_data:
             OfferDetail.objects.create(offer=offer, **detail_data)
