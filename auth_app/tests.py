f128266b7445bf9318f0b057a5e414b84e54f9ab
BenjaminTietz <mail@benjamin-tietz.com>
2024-11-23 10:55:38 +0100

    merge from main to ben

--- auth_app/tests.py
+++ auth_app/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from auth_app.models import CustomUser
 
-# Create your tests here.
+class RegisterViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_user_registration(self):
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password": "securepassword123",
+            "repeated_password": "securepassword123",
+            "type": "customer",
+        }
+        response = self.client.post('/api/registration/', data)
+        print("Response data:", response.data)  # Debugging
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(CustomUser.objects.get().username, "testuser")
