d505d649dee5bb3b0e589899fde70bcb1c8a2de1
Lukas Nolting <lukas.nolting@icloud.com>
2024-11-23 14:14:12 +0100

    updated settings.py

--- coderr/settings.py
+++ coderr/settings.py
@@ -159,11 +159,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'coderr/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'coderr/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
